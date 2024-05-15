e4a87af05e98f3b9cecea435b71733e8d5aa3134
Azizmemon786 <136925916+Azizmemon786@users.noreply.github.com>
2024-05-15 19:00:26 +0500

    new

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 var swiper = new Swiper(".childSwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
+
+function readMore() {
+  const dots = document.getElementById("dots");
+  const moreText = document.getElementById("more");
+  const btn = document.getElementById("btn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btn.innerHTML = "Read More";
+    moreText.style.display = "none";
+  }
+  else {
+    dots.style.display = 'none';
+    btn.innerHTML = "Read Less...";
+    moreText.style.display = "inline";
+  }
+}
