412b0737a5cc780856654ae9d30a78799981832c
Azizmemon786 <136925916+Azizmemon786@users.noreply.github.com>
2024-05-15 22:37:59 +0500

    desktop

--- js/script.js
+++ js/script.js
@@ -11,23 +11,3 @@ var swiper = new Swiper(".childSwiper", {
   },
 });
 
-
-
-
-
-// function readMore() {
-//   const dots = document.getElementById("dots");
-//   const moreText = document.getElementById("more");
-//   const btn = document.getElementById("btn");
-
-//   if (dots.style.display === "none") {
-//     dots.style.display = "inline";
-//     btn.innerHTML = "Read More";
-//     moreText.style.display = "none";
-//   }
-//   else {
-//     dots.style.display = 'none';
-//     btn.innerHTML = "Read Less...";
-//     moreText.style.display = "inline";
-//   }
-// }
